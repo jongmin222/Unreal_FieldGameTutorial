bee010fba715d596cdbd14f45135f0e60c182cdc
jongmin2 <min980518@gmail.com>
2023-01-08 18:37:12 +0900

    튜토리얼 공부
    변수, 타이머, 이벤트

---
+++ Source/FieldGameTutorial/Countdown.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/TextRenderComponent.h"
+#include "Countdown.generated.h"
+
+UCLASS()
+class FIELDGAMETUTORIAL_API ACountdown : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACountdown();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+private:
+	int32 CountdownTime;
+
+	UTextRenderComponent* CountdownText;
+
+	void UpdateTimerDisplay();
+
+	void AdvanceTimer();
+
+	UFUNCTION(BlueprintNativeEvent)
+	void CountdownHasFinished();
+	virtual void CountdownHasFinished_Immplementation();
+
+	FTimerHandle CountdownTimerHandle;
+};
